88a6fb46411aeb3957d49fa8310e6d0e5bb172a3
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-04-14 14:12:44 -0500

    primeros componentes e inicio de la estrcutura del proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shered/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shered/header/header.component';
+import { SidebarComponent } from './shered/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shered/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
