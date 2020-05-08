7c6b3f824647459f21fd559740698158910aa6e8
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-05-07 19:04:13 -0500

    configuración de imagen pipe

--- src/app/shered/sidebar/sidebar.component.ts
+++ src/app/shered/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService, UsuarioService } from '../../services/service.index';
+import { Usuario } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,9 +10,12 @@ import { SidebarService, UsuarioService } from '../../services/service.index';
 })
 export class SidebarComponent implements OnInit {
 
+  usuario: Usuario;
+
   constructor( public Ssidebar: SidebarService, public UusuarioService: UsuarioService) { }
 
   ngOnInit(): void {
+    this.usuario = this.UusuarioService.usuario;
   }
 
 }
