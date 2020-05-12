8c14f592f7c8dcc1c92d7b33233e63b6a170d7ef
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-05-11 20:13:05 -0500

    cargar usuarios de forma paginada

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
+import { UsuarioService } from '../../services/service.index';
 
 @Component({
   selector: 'app-usuarios',
@@ -8,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
-  constructor() { }
+  usuarios: Usuario[] = [];
+  desde: number = 0;
+
+  totalRegistros: number = 0;
+
+  constructor( public UusuarioService: UsuarioService ) { }
 
   ngOnInit(): void {
+    this.cargarUsuario();
+  }
+
+
+  cargarUsuario(){
+
+  this.UusuarioService.cargarUsuarios(this.desde)
+                      .subscribe( (response: any) => {
+                      console.log(response);
+                      this.totalRegistros = response.total_Usuarios;
+                      this.usuarios = response.usuarios;
+                      });
+
+  }
+
+  cambiarDesde( valor: number){
+
+    const desde = this.desde + valor;
+
+    console.log(desde);
+
+    if ( desde >= this.totalRegistros ){
+      return;
+    }
+    if ( desde < 0){
+      return;
+    }
+
+    this.desde += valor;
+    this.cargarUsuario();
+
   }
 
 }
