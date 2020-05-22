d8ebc3d791d524dafa317d65e95e38e80907d09a
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-05-22 18:39:14 -0500

    Busqueda usando el sercio e implementado en HTML

--- src/app/pages/busqueda/busqueda.component.ts
+++ src/app/pages/busqueda/busqueda.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { Usuario } from 'src/app/models/usuario.model';
+import { Medico } from 'src/app/models/medico.model';
+import { Hospital } from 'src/app/models/hospital.model';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,8 +14,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BusquedaComponent implements OnInit {
 
+  usuarios: Usuario[] = [];
+  medicos: Medico[] = [];
+  hospitales: Hospital[] = [];
+
+
   constructor(
     public activatedRoute: ActivatedRoute,
+    public http: HttpClient,
   ) {
 
     activatedRoute.params
@@ -18,12 +29,30 @@ export class BusquedaComponent implements OnInit {
 
           const termino = params.termino;
           console.log(termino);
+          this.buscar(termino);
 
         });
 
    }
 
   ngOnInit(): void {
+  }
+
+  buscar( termino: string ){
+
+     const url = URL_SERVICIOS + '/busqueda/todo/' + termino;
+
+     this.http.get( url )
+     .subscribe( (response: any) => {
+
+                console.log(response);
+
+                this.hospitales = response.hospitales;
+                this.medicos = response.medicos;
+                this.usuarios = response.usuarios;
+
+              });
+
   }
 
 }
